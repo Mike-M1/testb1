f5e59e4db1ca3ddf263caf86982641bedb2bc61c
Mikal Mckeever <mikalmckeever6@gmail.com>
2025-09-24 08:56:38 -0500

    Ai

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const client = new OpenAI({
 app.get("/hello", async (req, res) => {
     const response = await client.responses.create({
         model: "gpt-5",
-        input: 'Write a one-sentence story about a skateboarding badger'
+        input: 'You are an assistant that is going to be helping John Doe get to the bottom of his customers needs. He does remodeling and building. He will give you a series of questions to ask the customers you may or may not ask some based on what the answer. Let him know what you are going to do and assure him you are capable of helping the customers'
     });
     res.json({ messsage: response.output_text});
 });
